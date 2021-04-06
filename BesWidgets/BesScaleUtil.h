25a38a048425b56390514066f4ecfd23588ac527
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2021-04-06 11:11:15 +0800

    refactor: use QScreen instead of QDesktopWidget
    Class QDesktopWidget is obsolete.

--- BesWidgets/BesScaleUtil.h
+++ BesWidgets/BesScaleUtil.h
@@ -1,7 +1,7 @@
 ﻿#ifndef BESSCALEUTIL_H
 #define BESSCALEUTIL_H
 #include <QApplication>
-#include <QDesktopWidget>
+#include <QScreen>
 #include <QRect>
 
 class BesScaleUtil{
@@ -9,30 +9,30 @@ public:
     //按和最大宽的比例缩放
     static double scale()
     {
-        //获取可用桌面大小
-        QRect deskRect = getDeskRect();
+        //获取主屏幕可用大小
+        QRect primaryScreenRect = getPrimaryScreenRect();
 
         //窗口本身高800px，分辨率高小于800时缩放到一个固定比例。
         //              分辨率高大于800，小于1080的情况下不缩放
         //              分辨率高大于1080时，按比例缩放
 
-        if(deskRect.height() < 800)
+        if(primaryScreenRect.height() < 800)
             return 0.72;
-        else if(deskRect.height() <= 1080)
+        else if(primaryScreenRect.height() <= 1080)
             return 1.0;
         else
-            return deskRect.height() / 1080.0;
+            return primaryScreenRect.height() / 1080.0;
     }
 
-    static QRect getDeskRect()
+    static QRect getPrimaryScreenRect()
     {
 
-        QDesktopWidget* desktopWidget = QApplication::desktop();
+        QScreen* primaryScreen = QGuiApplication::primaryScreen();
 
-        //获取可用桌面大小
-        QRect deskRect = desktopWidget->availableGeometry();
+        //获取主屏幕可用大小
+        QRect primaryScreenRect = primaryScreen->availableGeometry();
 
-        return deskRect;
+        return primaryScreenRect;
     }
 };
 
