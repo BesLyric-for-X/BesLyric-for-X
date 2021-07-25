0176f370043d62338e9151cacbb2f82598953a75
BensonLaur <1035069088@qq.com>
2021-07-25 21:13:40 +0800

    fix: 纠正 BesLyric-for-X 适用操作系统的描述
    patch for 9e330217695c3f55b1bf8117db27971a7f2ef19a

--- MiddleWidgets/SettingWidget/SuDonation.cpp
+++ MiddleWidgets/SettingWidget/SuDonation.cpp
@@ -40,7 +40,7 @@ QWidget *SuDonation::getUnitWidget(QWidget *parent)
 
     label1->setText(tr("<p style='line-height:130%'>本软件的设计和开发源自 <a style='color:#666666;' href='https://www.cnblogs.com/BensonLaur/p/6262565.html'> BesLyric(使用SOUI界面框架实现) </a>, "
                        "自其发布后的一年余中，在广大网友的建议和默默支持下不断发展，可惜由于开发框架限制，只能在 Windows 下运行，效果也不是很理想。</p>"));
-    label2->setText(tr("<p style='line-height:130%'>也是在此背景下，<a style='color:#666666;' href='https://github.com/BesLyric-for-X/BesLyric-for-X'>BesLyric-for-X</a> 应运而生。新版本使用了跨平台框架实现，支持 Windows、Linux(Ubuntu)、macOS 操作系统。"
+    label2->setText(tr("<p style='line-height:130%'>也是在此背景下，<a style='color:#666666;' href='https://github.com/BesLyric-for-X/BesLyric-for-X'>BesLyric-for-X</a> 应运而生。新版本使用了跨平台框架实现，支持 Windows、Linux、macOS 操作系统。"
                        "另外，强大的应用基础框架使得新版本软件的实现效果更加地“网易云”，以为用户提供更好的使用体验！</p>"));
 
     label3->setText(tr("<p style='line-height:130%'>您的喜欢和支持，是本软件不断发展的动力，如果喜欢本软件，欢迎访问 <a style='color:#666666;' href='http://www.cnblogs.com/BensonLaur/p/6262565.html#title7'>打赏页面</a></p>"));
